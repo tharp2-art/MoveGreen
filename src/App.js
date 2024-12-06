63725629e131de7b867b3200997790bec9bbc551
tharp2-art <praveen.tharmalingam@students.bfh.ch>
2024-12-06 10:38:27 +0100

    Initial commit: MoveGreen sustainable urban mobility web app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import MapPage from './pages/MapPage';
+import Explorer from './pages/Explorer';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import CommunityPage from './pages/CommunityPage';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/karte" element={<MapPage />} />
+            <Route path="/explorer" element={<Explorer />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/profil" element={<Profile />} />
+            <Route path="/suche" element={<Search />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
